825641f4d4ad231d40b254b8732c97a176783a62
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-02-27 03:50:22 +0600

    Added a calculation of audio and video files, and printed them to console.

--- utilities.py
+++ utilities.py
@@ -5,13 +5,15 @@ from datetime import datetime
 
 def list_files(path, tree_file):
     """
-    Lists files in the given path.
-    Concept from: https://stackoverflow.com/a/9728478
+    Lists files in the given path. Concept from: https://stackoverflow.com/a/9728478
+    Additionally, prints a count of audio and video files in the console.
 
     :param path: The root. Tree starts to populate from here.
     :param tree_file: The file where the tree is to be written.
     :return: None
     """
+    count_audio = 0
+    count_video = 0
     with open(tree_file, 'w', encoding='utf-8') as tf:
         for root, dirs, files in os.walk(path):
             level = root.replace(path, '').count(os.sep)
@@ -19,13 +21,27 @@ def list_files(path, tree_file):
             tf.writelines('{}{}/'.format(indent, os.path.basename(root)) + '\n')
             sub_indent = ' ' * 4 * (level + 1)
             for f in files:
+                file_path = os.path.join(root, f)
                 if not f == 'desktop.ini':  # Ignore `desktop.ini` files
-                    file_path = os.path.join(root, f)
                     last_modified_timestamp = os.path.getmtime(file_path)
                     last_modified = datetime.fromtimestamp(
                         last_modified_timestamp).__str__()
                     tf.writelines('{}{}'.format(sub_indent, f) + ' ' +
                                   last_modified + '\n')
+                if file_path.endswith('.mp3') \
+                        or file_path.endswith('.wma') \
+                        or file_path.endswith('.flac') \
+                        or file_path.endswith('.wav'):
+                    count_audio += 1
+                elif file_path.endswith('.mp4') \
+                        or file_path.endswith('.mkv') \
+                        or file_path.endswith('.flv') \
+                        or file_path.endswith('.wmv') \
+                        or file_path.endswith('.mov') \
+                        or file_path.endswith('.avi'):
+                    count_video += 1
+    print('Number of audios: ' + count_audio.__str__())
+    print('Number of videos: ' + count_video.__str__())
 
 
 def format_files():
