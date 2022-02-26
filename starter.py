0f739e428f05403e78436925683cb01d1da2d17c
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-02-27 04:04:15 +0600

    Optimized an import.

--- starter.py
+++ starter.py
@@ -1,11 +1,11 @@
-import datetime
+from datetime import datetime
 from utilities import list_files, format_files
 
 # Root of directory
 ROOT = 'H:\My Drive\উত্তম সঙ্গীত'
 
 # Variables
-now = datetime.datetime.now().strftime('%Y_%m_%d_%H%M%S')
+now = datetime.now().strftime('%Y_%m_%d_%H%M%S')
 tree_name = 'tree_' + now
 tree_path = 'out\\' + tree_name + '.txt'
 
