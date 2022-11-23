bde8ad6d83f3164a1c5de9c513d17605f09bb758
Hasan Nahiyan Nobel <hasan.nahiyan.nobel@gmail.com>
2022-11-24 03:25:15 +0600

    Updated tree.

--- starter.py
+++ starter.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from utilities import list_files, format_files
 
 # Root of directory
-ROOT = 'H:\My Drive\উত্তম সঙ্গীত'
+ROOT = 'I:\My Drive\উত্তম সঙ্গীত'
 
 # Variables
 now = datetime.now().strftime('%Y_%m_%d_%H%M%S')
